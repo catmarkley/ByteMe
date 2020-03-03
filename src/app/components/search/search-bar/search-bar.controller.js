a382a52db7d4441e7c4d0d8610cc4c2803a8b900
Catherine Markley <catherinemarkley@Catherines-MacBook-Pro.local>
2020-03-02 20:54:01 -0500

    Added pantry component

--- src/app/components/search/search-bar/search-bar.controller.js
+++ src/app/components/search/search-bar/search-bar.controller.js
@@ -1,16 +1,15 @@
-function SearchBarController($state) {
+function SearchBarController(IngredientsService, $state) {
     var ctrl = this;
 
     ctrl.$onInit = function () {
       ctrl.searchText = '';
     }
 
-    ctrl.addIngr = function(event){
-      console.log("Clicked Add!")
+    ctrl.addIngr = function(event, searchText){
+      IngredientsService.addToPantry(searchText);
     }
 
     ctrl.findRecipe = function(event, searchText){
-      console.log("Clicked Find Recipe!")
       $state.go('results', {
         ingredient: searchText
       });
