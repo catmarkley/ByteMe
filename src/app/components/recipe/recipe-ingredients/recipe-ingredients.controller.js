fdf0c1d9f1a1afc286855c63726362f590a7ee88
Chandler Crane <ccrane2@nd.edu>
2020-03-03 18:49:43 -0500

    moved all data intake to http service

--- src/app/components/recipe/recipe-ingredients/recipe-ingredients.controller.js
+++ src/app/components/recipe/recipe-ingredients/recipe-ingredients.controller.js
@@ -4,34 +4,33 @@ function RecipeIngredientsController(RecipesService, IngredientsService, $state)
   console.log(recipeId);
 
   ctrl.$onInit = function () {
+    // Get the pantry list
     var pantry = IngredientsService.getPantry()
 
+
+    // Get the list of all recipes via HTTP
     RecipesService.getRecipes().then(function(recipes){
       recipes = recipes.data
-      console.log("pantry", pantry);
-      console.log("recipes", recipes);
       var ingredients = [];
       var i;
+
+      // Iterate over all recipes
       for(i in recipes){
         if(recipes[i]["id"] == recipeId){
           ingredients = recipes[i];
         }
       }
-      console.log(ingredients['name']);
       var name;
       name = ingredients['name'];
       var ingr;
       ingredients = ingredients['ingredients'];
       var x;
       var ingredient = [];
+      // Add name of ingredients to ingredients list
       for(x in ingredients){
         ingredient.push(ingredients[x]['name']);
-        //ingredient.push(ingredients['name'])
       }
-      //console.log(ingredient);
-      //for(ingr in ingredients['ingredients']['name']){
-      //  ingredient.push(ingr)
-      //}
+
       console.log(ingredient);
       ctrl.ingredients = ingredient;
       ctrl.name = name;
