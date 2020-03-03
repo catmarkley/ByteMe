094b5a2426dced68c1e6a22fee54d5bef1a4bf75
Emma <ebrady23@gmail.com>
2020-03-02 22:47:33 -0500

    display ingredients

--- src/app/components/recipe/recipe-ingredients/recipe-ingredients.controller.js
+++ src/app/components/recipe/recipe-ingredients/recipe-ingredients.controller.js
@@ -1,8 +1,38 @@
-function RecipeIngredientsController($state) {
+function RecipeIngredientsController(RecipesService, $state) {
   var ctrl = this;
-  
+  var recipeId = $state.params.id;
+  console.log(recipeId);
 
+  ctrl.$onInit = function () {
+    var data = RecipesService.getRecipes();
+    console.log(data);
+    var ingredients = [];
+    var i;
+    for(i in data){
+      if(data[i]["id"] == recipeId){
+        ingredients = data[i];
+      }
+    }
+    console.log(ingredients['name']);
+    var name;
+    name = ingredients['name'];
+    var ingr;
+    ingredients = ingredients['ingredients'];
+    var x;
+    var ingredient = [];
+    for(x in ingredients){
+      ingredient.push(ingredients[x]['name']);
+      //ingredient.push(ingredients['name'])
+    }
+    //console.log(ingredient);
+    //for(ingr in ingredients['ingredients']['name']){
+    //  ingredient.push(ingr)
+    //}
+    console.log(ingredient);
+    ctrl.ingredients = ingredient;
+    ctrl.name = name;
 
+  }
 }
 
 angular
