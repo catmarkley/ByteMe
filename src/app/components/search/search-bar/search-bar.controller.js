9a0c13a01c5092c331401e0e730741542161fada
Catherine Markley <catherinemarkley@catherines-mbp.dhcp.nd.edu>
2020-03-03 18:52:24 -0500

    Added comments

--- src/app/components/search/search-bar/search-bar.controller.js
+++ src/app/components/search/search-bar/search-bar.controller.js
@@ -2,19 +2,22 @@ function SearchBarController(IngredientsService, $state) {
     var ctrl = this;
 
     ctrl.$onInit = function () {
+      // Initializes the text in the search box
       ctrl.searchText = '';
     }
 
     ctrl.addIngr = function(event, searchText){
+      // Adds the text from the input box into the pantry using the ingredient service
       IngredientsService.addToPantry(searchText);
     }
 
     ctrl.findRecipe = function(event, searchText){
+      // Redirects to the results state (url: search/:ingredient)
+      // This state displays results based on what is in the search box
       $state.go('results', {
         ingredient: searchText
       });
     }
-
 
 }
 
--- src/app/components/search/search-results/search-results.controller.js
+++ src/app/components/search/search-results/search-results.controller.js
@@ -1,9 +1,11 @@
 function SearchResultsController(IngredientsService, $state) {
     var ctrl = this;
 
+    // Grab the ingredient parameter from the current page url
     var searchIngredient = $state.params.ingredient;
 
     ctrl.$onInit = function () {
+      // When the page loads, call the ingredients service and populate the recipe list
       ctrl.recipeId = '';
       var recipes = IngredientsService.getResults(searchIngredient);
       ctrl.recipes = recipes;
