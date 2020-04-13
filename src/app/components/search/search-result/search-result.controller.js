18beb55c6ca7eb14fb2b8e9dfe718f542a6ed214
Catherine Markley <catherinemarkley@Catherines-MacBook-Pro.local>
2020-04-13 13:53:32 -0400

    Fixed search result navigation

--- src/app/components/search/search-result/search-result.controller.js
+++ src/app/components/search/search-result/search-result.controller.js
@@ -2,15 +2,13 @@ function SearchResultController(IngredientsService, $state) {
     var ctrl = this;
 
     // When the 'Go To Recipe' button is clicked, send the event to the parent
+    // The parent, searchResults, deals with the actual navigation
     ctrl.goToRecipe = function(){
-        //ctrl.onNavigate({
-        //    $event: {
-        //        recipeId: ctrl.recipe.id
-        //    }
-        $state.go('recipe', {
-          recipeId: ctrl.recipe.id
-        })
-        //});
+        ctrl.onNavigate({
+            $event: {
+                recipeId: ctrl.recipe.id
+            }
+        });
     };
 
 }
